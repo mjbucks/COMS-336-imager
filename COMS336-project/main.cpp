2032cfb2e59980b17869089011ca8fe14edb74d9
Max <mrohrer@iastate.edu>
2024-09-12 16:40:33 -0500

    Made a sphere

--- COMS336-project/main.cpp
+++ COMS336-project/main.cpp
@@ -4,7 +4,22 @@
 #include "vec3.h"
 #include "ray.h"
 
+bool isSphereHit(const point3& center, double radius, const ray& r){
+    vec3 oc = center - r.origin();
+
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+
+    auto discriminant = b*b - 4*a*c;
+
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+    if (isSphereHit(point3(0, 0, 1), 0.5, r))
+        return color(1, 0, 0);
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
 
