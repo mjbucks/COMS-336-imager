8fda20d4630a381961cc8fe069bc80aa6b113d34
Max <mrohrer@iastate.edu>
2024-09-26 10:15:01 -0500

    finished antialiasing

--- COMS336-project/camera.h
+++ COMS336-project/camera.h
@@ -8,6 +8,7 @@ class camera {
 
         double aspect_ratio = 1.0;
         int image_width = 100;
+        int samples_per_pixel = 10;
 
         void render(const canbehit& world) {
             init();
@@ -17,15 +18,14 @@ class camera {
             for (int i = 0; i < image_height; i++) {
                 std::clog << "\rScanlines left: " << (image_height - i) << ' ' << std::flush;
                 for (int k = 0; k < image_width; k++) {
-                    auto pixel_center = pixel00_loc + (k * pixel_du) + (i * pixel_dv);
+                    color pixel_color(0, 0, 0);
 
-                    auto ray_dir = pixel_center - center;
+                    for(int sample = 0; sample < samples_per_pixel; sample++) {
+                        ray r = get_ray(k, i);
+                        pixel_color += ray_color(r, world);
+                    }
 
-                    ray r(center, ray_dir);
-
-                    color pixel_color = ray_color(r, world);
-
-                    write_color(std::cout, pixel_color);
+                    write_color(std::cout, pixel_samples_scale * pixel_color);
                 }
             }
             std::clog << "\rRender complete.";
@@ -34,6 +34,7 @@ class camera {
     private:
 
         int image_height;
+        double pixel_samples_scale;
         point3 center;
         point3 pixel00_loc;
         vec3 pixel_du;
@@ -44,6 +45,8 @@ class camera {
             image_height = int(image_width / aspect_ratio);
 
             image_height = (image_height < 1) ? 1 : image_height;
+
+            pixel_samples_scale = 1.0 / samples_per_pixel;
 
             center = point3(0, 0, 0);
 
@@ -60,6 +63,21 @@ class camera {
             auto vp_upper_left = center - vec3(0, 0, focul_len) - vp_u/2 - vp_v/2;
             pixel00_loc = vp_upper_left + 0.5 * (pixel_du + pixel_dv);
 
+        }
+
+        ray get_ray(int k, int i) {
+            auto offset = sample_square();
+
+            auto pixel_sample = pixel00_loc + ((k + offset.x()) * pixel_du) + ((i + offset.y()) * pixel_dv);
+
+            auto ray_origin = center;
+            auto ray_dir = pixel_sample - ray_origin;
+
+            return ray(ray_origin, ray_dir);
+        }
+
+        vec3 sample_square() const {
+            return vec3(random_double() - 0.5, random_double() - 0.5, 0);
         }
 
         color ray_color(const ray& r, const canbehit& world) const {
--- COMS336-project/commons.h
+++ COMS336-project/commons.h
@@ -1,6 +1,7 @@
 #ifndef COMMONS_H
 #define COMMONS_H
 
+#include <cstdlib>
 #include <cmath>
 #include <iostream>
 #include <limits>
@@ -14,6 +15,15 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+
+    return min + (max - min)*random_double();
 }
 
 #include "color.h"
