047644a1ebed727a4223f060fa6b432857627628
Max <mrohrer@iastate.edu>
2024-11-29 21:10:27 -0600

    Finished BVH

--- COMS336-project/bvh.h
+++ COMS336-project/bvh.h
@@ -18,7 +18,12 @@ class bvh_node : public canbehit {
     }
 
     bvh_node(std::vector<shared_ptr<canbehit>>& objects, size_t start, size_t end) {
-        int axis = random_int(0,2);
+        bbox = aabb::empty;
+
+        for (size_t object_index=start; object_index < end; object_index++)
+            bbox = aabb(bbox, objects[object_index]->bounding_box());
+
+        int axis = bbox.longest_axis();
 
         auto comparator = (axis == 0) ? box_x_compare
                         : (axis == 1) ? box_y_compare
@@ -38,8 +43,6 @@ class bvh_node : public canbehit {
             left = make_shared<bvh_node>(objects, start, mid);
             right = make_shared<bvh_node>(objects, mid, end);
         }
-
-        bbox = aabb(left->bounding_box(), right->bounding_box());
     }
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
