c518602ec216086700a33c358bcdf9bc2953b917
Max <mrohrer@iastate.edu>
2024-12-01 12:47:22 -0600

    FINISHED CUBES

--- COMS336-project/aabb.h
+++ COMS336-project/aabb.h
@@ -11,7 +11,10 @@ class aabb {
     aabb() {} // The default AABB is empty, since intervals are empty by default.
 
     aabb(const interval& x, const interval& y, const interval& z)
-      : x(x), y(y), z(z) {}
+      : x(x), y(y), z(z)
+    {
+        pad_to_minimums();
+    }
 
     aabb(const point3& a, const point3& b) {
         // Treat the two points a and b as extrema for the bounding box, so we don't require a
@@ -20,6 +23,8 @@ class aabb {
         x = (a[0] <= b[0]) ? interval(a[0], b[0]) : interval(b[0], a[0]);
         y = (a[1] <= b[1]) ? interval(a[1], b[1]) : interval(b[1], a[1]);
         z = (a[2] <= b[2]) ? interval(a[2], b[2]) : interval(b[2], a[2]);
+
+        pad_to_minimums();
     }
 
     aabb(const aabb& box0, const aabb& box1) {
@@ -67,6 +72,15 @@ class aabb {
     }
 
     static const aabb empty, universe;
+
+  private:
+
+    void pad_to_minimums() {
+        double delta = 0.0001;
+        if (x.size() < delta) x = x.expand(delta);
+        if (y.size() < delta) y = y.expand(delta);
+        if (z.size() < delta) z = z.expand(delta);
+    }
 };
 
 const aabb aabb::empty    = aabb(interval::empty,    interval::empty,    interval::empty);
